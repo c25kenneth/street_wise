35429bd7fd77f7d98afa8dba2b683f68734bafdd
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2022-01-15 08:43:38 -0800

    added other packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AndroidAlarmManager.initialize();
   runApp(MyApp());
 }
 
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutte Demo Home Page'),
     );
   }
 }
